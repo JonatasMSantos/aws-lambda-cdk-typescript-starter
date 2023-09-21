c7c54a20e24f6ca175633260b75fb91028be194f
JonatasMSantos <jts.msantos@gmail.com>
2023-09-21 14:12:14 -0300

    Initial commit

---
+++ test/aws-lambda-cdk-typescript-starter.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLambdaCdkTypescriptStarter from '../lib/aws-lambda-cdk-typescript-starter-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-lambda-cdk-typescript-starter-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLambdaCdkTypescriptStarter.AwsLambdaCdkTypescriptStarterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
